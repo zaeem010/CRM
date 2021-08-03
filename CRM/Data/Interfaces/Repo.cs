9cf9476ae316283bdfc2e342233c8d282512fd45
Zaeem khan <khanzaeem010@gmail.com>
2021-08-03 11:42:28 +0500

    Add project files.

---
+++ CRM/Data/Interfaces/Repo.cs
@@ -0,0 +1,87 @@
+﻿using CRM.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using CRM.Data;
+
+namespace CRM.Data.Repository
+{
+    public class Repo<T> : IRepository<T> where T : class
+    {
+        SqlConnection connection;
+        public T FindByID(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                return connection.Query<T>(query).FirstOrDefault();
+            }
+        }
+        
+        public IEnumerable<T> GetAllData(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                return connection.Query<T>(query).ToList();
+            }
+        }
+        public IEnumerable<T> GetAllDataSp(string query, SqlParameter[] Parrameters)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                return connection.Query<T>(query).ToList();
+            }
+        }
+
+        public int GetMaxId(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                return connection.Query<int>(query).FirstOrDefault();
+            }
+            //throw new NotImplementedException();
+        }
+        public string Getstring(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                return connection.Query<string>(query).FirstOrDefault();
+            }
+            //throw new NotImplementedException();
+        }
+        public void Save(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                connection.Query<T>(query);
+            }
+        }
+
+
+        public void Update(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                connection.Query<T>(query);
+            }
+        }
+
+        public void Delete(string query)
+        {
+            using (connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                connection.Query<T>(query);
+            }
+        }
+    }
+}
